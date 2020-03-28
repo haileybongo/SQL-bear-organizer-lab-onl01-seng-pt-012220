850bff0d28777901f19d0be0bc95e7de8b58cec9
Hailey Bongo <haileyebongo@gmail.com>
2020-03-28 19:27:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,15 @@
 INSERT INTO bears (name, age, gender, color, temerpament, alive) VALUES ("Mr. Chocolate", 15, "F", "brown", "friendly", 1);
+
+INSERT INTO bears (name, age, gender, color, temerpament, alive) VALUES ("Rowdy", 10, "M", "black", "calm", 1);
+
+INSERT INTO bears (name, age, gender, color, temerpament, alive) VALUES ("Tabitha", 9, "F", "brown", "anxious", 0);
+
+INSERT INTO bears (name, age, gender, color, temerpament, alive) VALUES ("Sergeant Brown", 20, "M", "brown", "friendly", 1);
+
+INSERT INTO bears (name, age, gender, color, temerpament, alive) VALUES ("Melissa", 15, "F", "brown", "calm", 1);
+
+INSERT INTO bears (name, age, gender, color, temerpament, alive) VALUES ("Grinch", 3, "M", "brown", "friendly", 1);
+
+INSERT INTO bears (name, age, gender, color, temerpament, alive) VALUES ("Wendy", 19, "F", "brown", "friendly", 0);
+
+INSERT INTO bears (name, age, gender, color, temerpament, alive) VALUES (NULL, 11, "M", "black", "mean", 1);
