87c04a2d707354338ea04156c2cf2bbd3501a2c3
Hailey Bongo <haileyebongo@gmail.com>
2020-03-28 19:49:46 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color COUNT(color) AS value_occurrence
+  "SELECT color, COUNT(color) AS value_occurrence
     FROM bears
     GROUP BY color
     ORDER BY value_occurrence DESC
